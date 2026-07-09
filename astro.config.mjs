d72122adc45696dd35e75b718444802bab100dbb
Matheus Boscariol <matheus@matheusbosc.com>
2026-07-09 15:56:03 -0400

    a

--- astro.config.mjs
+++
@@ -1,9 +0,0 @@
-import { defineConfig } from 'astro/config';
-
-import sitemap from '@astrojs/sitemap';
-
-// https://astro.build/config
-export default defineConfig({
-  site: 'https://matheusbosc.com',
-  integrations: [sitemap()]
-});
